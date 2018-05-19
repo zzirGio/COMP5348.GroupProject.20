77eb529712292828050f001fdf9105c4b82e6a5e
Unknown <smes3708@uni.sydney.edu.au>
2018-05-19 19:16:47 +1000

    wip: implementing delivery service
    TODO:
    
    - implement Subscription Service
    - implement Publish delivery outcome to VideoStore
    - implement Publish email to EmailService. Topic is 'SendEmail'

--- DeliveryCo.Application/DeliveryCo.Process/Program.cs
+++ DeliveryCo.Application/DeliveryCo.Process/Program.cs
@@ -9,14 +9,23 @@ using Microsoft.Practices.Unity.Configuration;
 using Microsoft.Practices.Unity.ServiceLocatorAdapter;
 using Microsoft.Practices.ServiceLocation;
 using System.Configuration;
+using Common;
+using DeliveryCo.Business.Components;
+using DeliveryCo.Process.SubscriptionService;
 
 namespace DeliveryCo.Process
 {
     class Program
     {
+        private static global::Common.SubscriberServiceHost mHost;
+        private const String cAddress = "net.msmq://localhost/private/DeliverCoQueueTransacted";
+        private const String cMexAddress = "net.tcp://localhost:9019/DeliverCoQueueTransacted/mex";
+
         static void Main(string[] args)
         {
             ResolveDependencies();
+            HostSubscriberService();
+            SubscribeForEvents();
             using (ServiceHost lHost = new ServiceHost(typeof(DeliveryService)))
             {
                 lHost.Open();
@@ -35,6 +44,17 @@ namespace DeliveryCo.Process
             lSection.Containers["containerOne"].Configure(lContainer);
             UnityServiceLocator locator = new UnityServiceLocator(lContainer);
             ServiceLocator.SetLocatorProvider(() => locator);
+        }
+
+        private static void HostSubscriberService()
+        {
+            mHost = new SubscriberServiceHost(typeof(SubscriberService), cAddress, cMexAddress, true, ".\\private$\\DeliverCoQueueTransacted");
+        }
+
+        private static void SubscribeForEvents()
+        {
+            SubscriptionServiceClient lClient = new SubscriptionServiceClient();
+            lClient.Subscribe("OrderDelivery", cAddress);
         }
     }
 }
---
+++ DeliveryCo.Business/DeliveryCo.Business.Components/SubcriberService.cs
@@ -0,0 +1,22 @@
+﻿using DeliveryCo.Business.Components.Interfaces;
+
+namespace DeliveryCo.Business.Components
+{
+    public class SubscriberService : ISubscriberService
+    {
+        public void PublishToSubscriber(Common.Model.Message pMessage)
+        {
+            if (pMessage is OrderDeliveryMessage)
+            {
+                OrderDeliveryMessage lMessage = pMessage as OrderDeliveryMessage;
+                SendEmailMessageToEmailMessage lVisitor = new SendEmailMessageToEmailMessage();
+                pMessage.Accept(lVisitor);
+
+                ServiceLocator.Current.GetInstance<IDeliveryProvider>().SubmitDelivery(
+                    MessageTypeConverter.Instance.Convert<
+                        global::EmailService.MessageTypes.EmailMessage,
+                        global::EmailService.Business.Entities.EmailMessage>(lVisitor.Result));
+            }
+        }
+    }
+}
