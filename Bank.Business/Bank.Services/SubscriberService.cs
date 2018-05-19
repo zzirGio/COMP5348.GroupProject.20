24b4980b30cab87dd36a7946b86d3d69582e3ff6
Karthik <balasu11@msu.edu>
2018-05-19 17:46:14 +1000

    Bank ready to test
    Bank has been subscribed to and publishes responses back.

---
+++ Bank.Business/Bank.Services/SubscriberService.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Services
+{
+    public class SubscriberService : ISubscriberService
+    {
+        public void PublishToSubscriber(Message pMessage)
+        {
+            TransferRequestMessage message = pMessage as TransferRequestMessage;
+            TransferService tService = new TransferService();
+            tService.Transfer(message.Amount, message.FromAccountNumber, message.ToAccountNumber);
+        }
+    }
+}
---
+++ Common/Model/TransferRequestMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Common.Model
+{
+    [DataContract]
+    public class TransferRequestMessage : Message
+    {
+        [DataMember]
+        public double Amount { get; set; }
+
+        [DataMember]
+        public int FromAccountNumber { get; set; }
+
+        [DataMember]
+        public int ToAccountNumber { get; set; }
+    }
+}
